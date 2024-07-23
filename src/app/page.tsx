5530fa01ba182ffd86152baa0c51d13529521b3c
Danimal01 <danialdadkhoo@gmail.com>
2024-07-23 19:56:32 -0400

    sheeesh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   const handleButtonClick = () => {
-    const url = "https:/offramp-sandbox.gatefi.com/?merchantId=9e34f479-b43a-4372-8bdf-90689e16cd5b&cryptoCurrency=ETH&payout=SPEI&fiatCurrency=MXN&region=MX&wallet=0xc458f721D11322E36f781a9C58055de489178BF2&walletLock=true&fiatCurrencyLock=true&cryptoCurrencyLock=true&fiatAmount=100&cryptoAmountLock=true&confirmRedirectUrl=https://offramp-flow-evm.vercel.app/confirm/";
+    const url = "https://offramp-sandbox.gatefi.com/?merchantId=9e34f479-b43a-4372-8bdf-90689e16cd5b&cryptoCurrency=ETH&payout=SPEI&fiatCurrency=MXN&region=MX&wallet=0xc458f721D11322E36f781a9C58055de489178BF2&walletLock=true&fiatCurrencyLock=true&cryptoCurrencyLock=true&fiatAmount=100&cryptoAmountLock=true&confirmRedirectUrl=https://offramp-flow-evm.vercel.app/confirm/";
     window.open(url, '_blank');
   };
 
